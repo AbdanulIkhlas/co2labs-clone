fdd61cb02a69d0ba4c7d891fa89c8031a69b16d7
AbdanulIkhlas <muhabdanulikhlas0983@gmail.com>
2025-04-19 16:57:57 +0700

    create hero section for it support

--- src/app/service/it-support/page.js
+++ src/app/service/it-support/page.js
@@ -1,5 +1,13 @@
+import ServicesHeroSection from "@/app/components/sections/ServicesHeroSection";
+
 export default function Page() {
-    return (
-        <div>IT Support</div>
-    );
+  return (
+    <div>
+      <ServicesHeroSection
+        urlImage="/img/hero-it-support.png"
+        textTitle="IT Support Services"
+        textSubtitle="“Comprehensive IT Support Solutions”"
+      />
+    </div>
+  );
 }
