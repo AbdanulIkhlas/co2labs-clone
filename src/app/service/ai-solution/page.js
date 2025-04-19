988e56068e571024fde82797f0fa7d49b89b7af1
AbdanulIkhlas <muhabdanulikhlas0983@gmail.com>
2025-04-19 17:03:14 +0700

    create hero section for ai solution page

--- src/app/service/ai-solution/page.js
+++ src/app/service/ai-solution/page.js
@@ -1,3 +1,13 @@
+import ServicesHeroSection from "@/app/components/sections/ServicesPage/ServicesHeroSection";
+
 export default function Page() {
-  return <div>AI Solution</div>;
+  return (
+    <div>
+      <ServicesHeroSection
+        urlImage="/img/hero-ai-solution.png"
+        textTitle="Third Party AI Tools"
+        textSubtitle="“Harness the Power of Third-Party AI Tools”"
+      />
+    </div>
+  );
 }
