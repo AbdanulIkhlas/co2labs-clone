20c8c4a9bd9e0f89c114cfb53895320d27736bfa
AbdanulIkhlas <muhabdanulikhlas0983@gmail.com>
2025-04-19 09:16:31 +0700

    update hero section for tablet and dekstop view

--- src/app/components/fragments/HeroImage.js
+++ src/app/components/fragments/HeroImage.js
@@ -1,12 +1,18 @@
 import Image from "next/image";
 
-const HeroImage = ({srcHeroImg}) => {
-  return <div className="relative">
-    <Image src={srcHeroImg} width={250} height={250} alt="Hero Image" 
-    className="absolute rounded-xl top-15 left-12 z-20" />
-    <Image src="/img/backdrop.png" width={220} height={220} alt="Backdrop"
-    className="absolute top-0 right-0 z-0" />
-  </div>;
+const HeroImage = ({ srcHeroImg }) => {
+  return (
+    <div className="relative">
+      <div className="w-[250px] h-[150px] absolute rounded-xl top-15 left-12 z-20 md:w-[390px] md:h-[235px] md:top-44 md:left-55
+      lg:left-0 lg:top-30 xl:left-60">
+        <Image src={srcHeroImg} fill alt="Hero Image" className="rounded-xl" />
+      </div>
+      <div className="w-[230px] h-[268px] absolute top-0 right-0 z-0 md:w-[360px] md:h-[440px] md:top-18
+      lg:top-0 lg:w-[400px] lg:h-[480px] xl:-top-1">
+        <Image src="/img/backdrop.png" fill alt="Backdrop" className="" />
+      </div>
+    </div>
+  );
 };
 
 export default HeroImage;
--- src/app/components/sections/LandingPage/HeroSection.js
+++ src/app/components/sections/LandingPage/HeroSection.js
@@ -3,16 +3,18 @@ import ButtonCustom from "../../elements/ButtonCustom";
 
 const HeroSection = () => {
   return (
-    <section className="border border-black -mt-5 pb-20">
-      <div className="h-68">
+    <section className="-mt-6 pb-20 md:pb-30 lg:flex lg:flex-row-reverse lg:py-110 xl:py-28">
+      <div className="h-68 md:h-130 lg:w-[52%]">
         <HeroImage srcHeroImg="/img/heroimg-landingpage.png" />
       </div>
-      <div className="px-10">
-        <h1 className="font-orbitron font-bold text-3xl">Where Code Meets</h1>
-        <h2 className="bg-secondary text-main font-orbitron font-bold mt-3 text-3xl w-max px-5">
+      <div className="px-10 lg:w-[48%] lg:px-0 lg:ps-30 lg:pt-16 xl:pt-27 xl:ps-26">
+        <h1 className="font-orbitron font-bold text-3xl md:text-5xl">
+          Where Code Meets
+        </h1>
+        <h2 className="bg-secondary text-main font-orbitron font-bold mt-3 text-3xl w-max px-5 md:text-5xl md:mt-4">
           Creativity
         </h2>
-        <p className="font-generalSans mt-3 text-xl">
+        <p className="font-generalSans mt-3 text-xl md:mt-5">
           Harnessing the power of our Coding Collective to build solutions for
           your business needs.
         </p>
