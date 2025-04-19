2bd83757ea963792ee69876865f2f598f047727f
AbdanulIkhlas <muhabdanulikhlas0983@gmail.com>
2025-04-19 16:17:54 +0700

    Add animation transition for some component

--- src/app/components/sections/LandingPage/AboutUsSection.js
+++ src/app/components/sections/LandingPage/AboutUsSection.js
@@ -1,38 +1,47 @@
 import Image from "next/image";
 import TeamCard from "../../fragments/TeamCard";
 import teamsData from "../../../data/teamsData";
+import AnimateOnScroll from "../../fragments/AnimatedOnScroll";
 
 const AboutUs = () => {
   return (
     <section className="bg-main text-center pb-10">
       <div className="px-10 lg:flex lg:py-120 lg:px-26 lg:gap-4">
         <div className="lg:w-1/2 lg:pe-10">
-          <h1 className="font-orbitron font-bold text-5xl md:mt-4 lg:text-right lg:-mt-4 xl:mt-24">
-            About Us
-          </h1>
-          <h3 className="font-generalSans text-xl text-left mt-8 mb-12 md:mb-6 lg:text-right
-          lg:mt-14 lg:font-medium lg:mb-18">
-            At CO2 Labs, we strive to provide simple yet powerful software
-            solutions and expert consultation to help your business thrive in
-            the digital world.
-          </h3>
-          <Image
-            src="/svg/line-curve.svg"
-            width={300}
-            height={300}
-            alt="Line Curve"
-            className="mb-8 md:w-full md:mb-0.5"
-          />
+          <AnimateOnScroll direction="up" delay={0.4}>
+            <h1 className="font-orbitron font-bold text-5xl md:mt-4 lg:text-right lg:-mt-4 xl:mt-24">
+              About Us
+            </h1>
+          </AnimateOnScroll>
+          <AnimateOnScroll direction="up" delay={0.6}>
+            <h3 className="font-generalSans text-xl text-left mt-8 mb-12 md:mb-6 lg:text-right
+            lg:mt-14 lg:font-medium lg:mb-18">
+              At CO2 Labs, we strive to provide simple yet powerful software
+              solutions and expert consultation to help your business thrive in
+              the digital world.
+            </h3>
+          </AnimateOnScroll>
+          <AnimateOnScroll direction="up" delay={0.8}>
+            <Image
+              src="/svg/line-curve.svg"
+              width={300}
+              height={300}
+              alt="Line Curve"
+              className="mb-8 md:w-full md:mb-0.5"
+            />
+          </AnimateOnScroll>
         </div>
-        <div className="lg:w-1/2 lg:p-6">
-          <Image
-            src="/img/about-img.png"
-            width={300}
-            height={300}
-            alt="Backdrop"
-            className="rounded-xl md:w-full"
-          />
-        </div>
+        <AnimateOnScroll direction="up" delay={0.4}>
+          <div className="lg:w-1/2 lg:p-6">
+            <Image
+              src="/img/about-img.png"
+              width={300}
+              height={300}
+              alt="Backdrop"
+              className="rounded-xl md:w-full"
+            />
+          </div>
+        </AnimateOnScroll>
       </div>
       <div id="about">
         <h1 className="font-orbitron font-bold text-5xl text-left mt-42 px-10 lg:ps-28">
--- src/app/components/sections/LandingPage/ServicesSection.js
+++ src/app/components/sections/LandingPage/ServicesSection.js
@@ -1,21 +1,26 @@
 import CardService from "../../fragments/CardService";
 import servicesData from "../../../data/servicesData";
+import AnimateOnScroll from "../../fragments/AnimatedOnScroll";
 
 const ServicesSection = () => {
   return (
     <section className="bg-secondary text-main px-12 py-40 md:px-24 lg:px-14 xl:px-38">
-      <h1 className="font-orbitron font-bold text-5xl md:text-5xl leading-18 text-left mb-5 md:text-center xl:text-left
-      xl:ps-4">
+      <h1
+        className="font-orbitron font-bold text-5xl md:text-5xl leading-18 text-left mb-5 md:text-center xl:text-left
+      xl:ps-4"
+      >
         Our Services
       </h1>
 
       <div className="flex flex-wrap justify-center gap-3">
-        {servicesData.map((service) => (
+        {servicesData.map((service, index) => (
           <div
             key={service.id}
             className="w-full sm:w-[calc(50%-0.75rem)] lg:w-[calc(33.333%-1rem)] xl:w-[calc(25%-1.125rem)] max-w-sm"
           >
-            <CardService {...service} />
+            <AnimateOnScroll direction="up" delay={index * 0.3}>
+              <CardService {...service} />
+            </AnimateOnScroll>
           </div>
         ))}
       </div>
